9efb0e7ef79d2a75567f46c37483277f190a03ae
Decoi <71142516+decoi444@users.noreply.github.com>
2024-05-18 16:38:17 +0100

    Update tkgrabber.c

--- tkgrabber.c
+++ tkgrabber.c
@@ -53,7 +53,7 @@ namespace pull
                 }
                 else
                 {
-                   // fuck....
+                   
                 }
                 Console.WriteLine();
             }
